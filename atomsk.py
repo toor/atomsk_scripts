62492ac5d7f0efcf0e54211e8b23baa4ec42e4ef
toor <william@huddie.net>
2022-06-10 11:27:58 +0100

    Truncate lattice parameter to 3d.p., and working on moving to subprocess module

--- atomsk.py
+++ atomsk.py
@@ -2,6 +2,14 @@ import os
 import subprocess
 import numpy as np
 import sys
+import math
+
+def truncate(num, digits) -> float:
+    dec = len(str(num).split('.')[1])
+    if dec <= digits:
+        return num
+    step = 10.0 ** digits
+    return math.trunc(step*num) / step
 
 # This function takes in a parameter which specifies the distance between neighbouring
 # atoms, and returns a dictionary of allowed unit cell types and relevant parameters
@@ -39,68 +47,56 @@ def generate_cells(d_nbr):
 # This generates the correct unit cell for a specified element,
 # atomic spacing and lattice type/orientation.
 # Returns: the filename of the relevant XSF file produced as well as the
-def generate_unit_cell(d_nbr, element, lattice_type):
+def generate_unit_cell(input_file, d_nbr, element, lattice_type):
     cells = generate_cells(d_nbr)
     
     # Extract information about the relevant cell
     orient = "orient " + cells[lattice_type]["orient"]
-    a = cells[lattice_type]["a"]
+    a = truncate(cells[lattice_type]["a"], 3)
     d_plane = cells[lattice_type]["d_plane"]
 
     lattice = lattice_type.split('_')[0]
 
-    filename = lattice_type + "_" + element + ".xsf"
-    rm = "rm " + filename
-    os.system(rm)
-
-    args = ["atomsk --create",
+    args = ["atomsk",
+            " --create ",
             lattice,
             str(a),
             element,
             orient,
-            filename]
+            " -fractional ",
+            input_file,
+            "-nthreads",
+            "1"]
     cmd = ' '.join(args)
+    print("atomsk: Generating unit cell")
+    print(cmd)
+    subprocess.run(args)
+
+    return d_plane
+
+def generate_supercell(input_file, output_file, d_plane, layers):   
+    args = ["atomsk ",
+            input_file,
+            " -duplicate 1 1 ",
+            str(layers),
+            " -fractional ",
+            output_file]
+    cmd = ' '.join(args)
+    print("atomsk: Generating supercell.")
     print(cmd)
 
-    os.system(cmd)
-
-    return filename, d_plane
-
-def generate_supercell(filename, d_plane, layer):
-    cut_plane =  (float(layer) - 1)*d_plane + 0.01
-    cell_z = float(layer)*d_plane
-    
-    filename2 = filename.split('.')[0] + "_" + str(layer) + ".xsf"
-    # remove annoying duplicates
-    rm = "rm " + filename2
-    os.system(rm)
-
-    args = ["atomsk " + filename,
-            "-duplicate 1 1",
-            str(layer),
-            #"-cut above ",
-            #str(cut_plane),
-            #"z",
-            #"-cell set ",
-            #str(d_plane),
-            #"H3",
-            #"-fractional",
-            filename2]
-    cmd = ' '.join(args)
-    print(cmd)
-
-    os.system(cmd)
+    #subprocess.run(args)
 
     return
 
 # Argument format: Smallest number of monolayers, largest number of monolayers,
 # lattice type/orientaion (specified in the format <lattice_type>_orientation)
 def main():
-    if len(sys.argv[1:]) < 5:
-        print("Expected 5 arguments, but only " + str(len(sys.argv) - 1) + " were given.")
+    args = sys.argv[1:]
+
+    if len(args) < 5:
+        print("Expected 5 arguments, but only " + str(len(args)) + " were given.")
         sys.exit(0)
-    args = sys.argv[1:] 
-    
     thickness1 = int(args[0])
     thickness2 = int(args[1])
     
@@ -108,10 +104,13 @@ def main():
     d_nbr = float(args[3])
     element = args[4]
     
-    filename, d_plane = generate_unit_cell(d_nbr, element, lattice_type)
+    input_file = lattice_type + "_" + element + ".cfg"
+
+    d_plane = generate_unit_cell(input_file, d_nbr, element, lattice_type)
     
     for j in range(thickness1, thickness2 + 1):
-        generate_supercell(filename, d_plane, j)
+        output_file = input_file.split('_')[0] + "_" + str(j) + ".cfg"
+        generate_supercell(input_file, output_file, d_plane, j)
 
     return
 
