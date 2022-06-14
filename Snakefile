c031e214d0541d93583713f8fcd4f50062ce0d0a
toor <william@huddie.net>
2022-06-14 11:47:32 +0100

    JAMS conversion working

--- Snakefile
+++ Snakefile
@@ -13,29 +13,17 @@ CELLS = ["sc_100",
 
 rule all:
     input:
-        expand("{cell}_{constant}_{layer}.cfg", cell=CELLS, constant=2.5, layer=layers)
-
+        expand("{cell}_{constant}_{layer}JAMS.cfg", cell=CELLS, constant=2.5, layer=layers)
 # NOTE: The constant passed here is the nearest-neighbour distance
 # between atomic sites - NOT the lattice constant/lattice parameter a.
 # The conversion is done in `atomsk.py`
 rule make_supercell:
     output:
-        "{cell}_{constant}_{layer}.cfg"
+        "{cell}_{constant}_{layer}JAMS.cfg"
     params:
+        element="Fe",
         cell=lambda wc: wc.cell,
         constant=lambda wc: wc.constant,
         layer=lambda wc: wc.layer
     script:
         "atomsk.py"
-
-#rule cleanup:
-    #shell:
-        #"rm *.cfg"
-
-#rule make_jams_cell:
-#    input:
-#        "{cell}_{constant}_{layer}.cfg"
-#    output:
-#        "{cell}_{constant}_{layer}_JAMS.cfg"
-#    script:
-#        "make_jams_cfg.py"
--- atomsk.py
+++ atomsk.py
@@ -2,8 +2,7 @@ import subprocess
 import numpy as np
 import sys
 import math
-
-element = "Fe"
+import re
 
 def truncate(num, digits) -> float:
     dec = len(str(num).split('.')[1])
@@ -48,11 +47,10 @@ def generate_cells(d_nbr):
 # This generates the correct unit cell for a specified element,
 # atomic spacing and lattice type/orientation.
 # Returns: the filename of the relevant XSF file produced as well as the
-def generate_unit_cell(input_file, d_nbr, lattice_type):
+def generate_unit_cell(input_file, d_nbr, lattice_type, element):
     cells = generate_cells(d_nbr)
 
     orient = cells[lattice_type]["orient"].split(' ')
-    print(orient)
     a = truncate(cells[lattice_type]["a"], 3)
     d_plane = cells[lattice_type]["d_plane"]
 
@@ -68,7 +66,6 @@ def generate_unit_cell(input_file, d_nbr, lattice_type):
             orient[1],
             orient[2],
             "-ow",
-            "-fractional",
             input_file,
             "cfg"]
     # convert to XCrysDen XSF format for easy conversion
@@ -76,10 +73,11 @@ def generate_unit_cell(input_file, d_nbr, lattice_type):
     convert_unit_cell_xsf = ["atomsk",
             input_file,
             "-ow",
+            "-fractional",
             "xsf"]
 
     subprocess.run(make_unit_cell)
-    subprocess.run(convert_unit_cell_xsf)
+    #subprocess.run(convert_unit_cell_xsf)
 
     return d_plane
 
@@ -101,30 +99,113 @@ def generate_supercell(input_file, output_file, d_plane, layers):
             "set",
             cell_z,
             "H3",
-            "-fractional",
             "-ow",
             output_file,
             "cfg"]
 
     convert_supercell_xsf = ["atomsk",
             output_file,
+            "-fractional",
             "-ow",
             "xsf"]
 
     subprocess.run(make_supercell)
     # convert to an XSF file for visualisation and easy conversion
     # to the relevant file format
-    subprocess.run(convert_supercell_xsf)
+    #subprocess.run(convert_supercell_xsf)
     return
 
+def read_unit_cell(filename, element):   
+    a1 = []
+    a2 = []
+    a3 = []
+    lattice_vectors = [a1, a2, a3]
+    coords = []
+
+    # this function should read the correct parameters for a unit cell
+    with open(filename) as f:
+        lines = [line for line in f]
+
+        header_start = 0
+        atom_pos_start = 0
+
+        for j in range(0, len(lines)):
+            # check if line contains equals sign and whether
+            # header_start has been updated yet. This if statement
+            # should only be entered if the latter is zero.
+            # Lattice vectors are guaranteed to always appear directly after
+            # the definition of the Angstrom
+            cond1 = re.search("A = ", lines[j]) 
+            #cond2 = re.search("#", lines[j])
+            if bool(cond1) and (not header_start):
+                # Lattice vectors appear directly after the declaration of the
+                # Angstrom unit.
+                header_start = j + 1
+            # same spiel here - the second condition is not strictly
+            # necessary
+            if (lines[j].strip() == element) and not atom_pos_start:
+                atom_pos_start = j + 1
+                print("Positions of atoms start at line: " + str(atom_pos_start))
+
+        # List slicing is exclusive
+        a = lines[header_start:(header_start + 3)]
+        b = lines[(header_start + 3):(header_start + 6)]
+        c = lines[(header_start + 6):(header_start + 9)]
+       
+        positions = lines[atom_pos_start:len(lines)]
+        for pos in positions:
+            coords.append(pos.strip('\t\n\r').split('    '))
+
+        for i, x in enumerate([a, b, c]):
+            lattice_vectors[i].append(str(float(x[0].split('=')[1].strip())))
+            lattice_vectors[i].append(str(float(x[1].split('=')[1].strip())))
+            lattice_vectors[i].append(str(float(x[2].split('=')[1].strip()))) 
+
+        return a1, a2, a3, coords           
+
+def convert_jams(element, a, input_file, output_file):
+    A = 1e-10 # 1 angstrom
+    a1, a2, a3, coords = read_unit_cell(input_file, element)
+
+    a1 = ', '.join(a1)
+    a2 = ', '.join(a2)
+    a3 = ', '.join(a3)
+   
+    with open(output_file, "w") as f:
+        f.write("unitcell = {\n")
+        f.write("  parameter = " + str(float(a)*A) + ";\n")
+        f.write("\n")
+        f.write("  basis = (\n")
+        f.write("    [" + a1 + "],\n")
+        f.write("    [" + a2 + "],\n")
+        f.write("    [" + a3 + "]);\n")
+        f.write("\n")
+        f.write("  positions = (\n")
+        
+        for j in range(0, (len(coords) - 1)):
+            coord = ', '.join(coords[j])
+            f.write("    (\"Fe\", [" + str(coord) + "]),\n")
+        f.write("    (\"Fe\", [" + str(coord) + "]));\n")
+        f.write("};")
+        f.close()
+        return
 # Argument format: Smallest number of monolayers, largest number of monolayers,
 # lattice type/orientaion (specified in the format <lattice_type>_orientation)
-def main(lattice_type, a, layers, output_file):   
+def main(element, lattice_type, a, layers, output_file):   
     input_file = lattice_type + "_" + element + ".cfg"
 
-    d_plane = generate_unit_cell(input_file, float(a), lattice_type)
+    d_plane = generate_unit_cell(input_file, float(a), lattice_type, element)
     generate_supercell(input_file, output_file, d_plane, int(layers))
     
+    input_file = output_file
+    output_file = output_file.split('.')[0] + "JAMS" + ".cfg"
+
+    convert_jams(element, a, input_file, output_file)
+
     return
 
-main(snakemake.params[0], snakemake.params[1], snakemake.params[2], snakemake.output[0])
+main(snakemake.params[0],
+    snakemake.params[1],
+    snakemake.params[2],
+    snakemake.params[3],
+    snakemake.output[0])
