d354db7886c02cd8dbddfd72da438ea616d12c52
toor <william@huddie.net>
2022-06-21 17:14:16 +0100

    Search for temperature directories

--- process_mag_data.py
+++ process_mag_data.py
@@ -3,12 +3,13 @@ import numpy as np
 import scipy
 import pandas as pd
 import matplotlib.pyplot as plt
+import re
 
 def plot_mag_vs_timesteps(filename):
     data = pd.read_csv(filename, sep=r"[ \t]+") 
     data.info()
 
-    d = {'Time': data['time'], 'm_z': data['Fe_mz']}
+    d = {'Time': data['time'], 'm_z': data['Fe_m']}
     df = pd.DataFrame(data=d)
 
     fig, ax = plt.subplots()
@@ -16,18 +17,17 @@ def plot_mag_vs_timesteps(filename):
     df.plot('Time', 'm_z', 'scatter', ax)
     plt.savefig('mag_vs_timesteps.png')
 
-def plot_mag_vs_temperature(lattice_type):
-    # list of paths to layers
-    for (path, layers, files) in os.walk(lattice_type):
-        print("path = " + path)
-        for layer in layers:
-            print("layer =" + layer)
-            for (path, temperatures, files) in os.walk(layer):
-                for temperature in temperatures:
-                    T = float(temperature.split("K"))
+def plot_mag_vs_temperature(lattice_type, layers):
+    for layer in layers:
+        print(layer)
+        _dir = lattice_type + "/" + str(layer)
+            
+        print("Walking " + _dir)
 
-                    filename = lattice_type
+        for path, dirnames, filenames in os.walk(_dir):
+            print(path)
+            true = bool(re.search(r"^.*K.*$", path))
+            if true:
+                print("i have entered this if statement, hallelujah")
 
-                    return
-
-plot_mag_vs_temperature("fcc_111")
+plot_mag_vs_temperature("fcc_111", [2,3,4,5,6])
