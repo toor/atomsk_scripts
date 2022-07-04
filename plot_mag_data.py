33216abe46d6d44b5a9dd4aaecf8fcf958385453
toor <william@huddie.net>
2022-07-04 11:18:31 +0100

    Add Bloch's law fitting

--- plot_mag_data.py
+++ plot_mag_data.py
@@ -1,31 +1,77 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.constants import k, m_e, hbar, e
+
+bohr = e*hbar/(2*m_e)
 
 sep = '\t'
 
-input_file = snakemake.input[0]
-output_file = snakemake.output[0]
+input_file = str(snakemake.input)
+output_file = str(snakemake.output)
 
+lattice = snakemake.params[0]
+layer = int(snakemake.params[1])
+constant = float(snakemake.params[2])
 
-#data = np.genfromtxt(input_file, sep)
+print(layer)
+
+d_planes = {
+    "sc_100": constant,
+    "bcc_100": constant/2,
+    "bcc_110": constant/np.sqrt(2),
+    "fcc_100": constant/2,
+    "fcc_111": constant/np.sqrt(3)
+}
+
+# Bloch's law in thin films
+def bloch(T):
+    # volume of Wigner-Seitz unit cell (1st BZ)
+    v_ws = 2*np.pi/np.power(constant, 3)
+    L_z = layer*d_planes[lattice]
+    
+    # TODO make all of these parameters in the Snakefile, to also pass to e.g. jinja
+    # Exchange
+    J = 2e-21
+    # TODO: Which value to use for the total spin? How is this calculated? am I just being thick?
+    S = 0.5
+    # Spinwave stiffness
+    D = 2*J*S*(constant**2)
+
+    prefactor = v_ws*k*T/(4*np.pi*S*L_z*D) 
+
+    array = np.zeros((layer, 1))
+    for i in range(0, layer):
+        k_z = i*np.pi/(constant*(layer - 1))
+
+        exp = np.exp((-D/(k*T))*np.power(k_z, 2))
+        print("exp = " + str(exp))
+        array[i] = np.log(1 - exp)
+    
+    m_z = 1 + prefactor*np.sum(array)
+
+    return m_z
 
 data = []
+
 with open(input_file) as f:
     for line in f:
         l = [float(i) for i in line.strip().split(sep)]
         data.append(l)
-
 data = np.array(data)
 
 plt.figure()
 
-plt.title("blah")
-plt.xlabel("Temperature")
-plt.ylabel("Magnetisation")
+title = "|M_z| vs. Temperature for "
 
-# Extract 1st and 2nd columns
+plt.title("|M_z| vs. Temperature.")
+plt.xlabel("Temperature (K)")
+plt.ylabel("Magnetisation (T)")
+
 temps = data[:,0]
 mags = data[:,1]
 
-plt.plot(temps, mags)
+bloch_data = np.array([bloch(T) for T in temps])
+
+plt.plot(temps, mags, label='JAMS data')
+plt.plot(temps, bloch_data, label='Bloch law')
 plt.savefig(output_file)
--- process_mag_data.py
+++ process_mag_data.py
@@ -4,26 +4,24 @@ import scipy
 import pandas as pd
 import matplotlib.pyplot as plt
 import re
-import pymbar
 
-TEMP_REGEX = r'[+-]?([0-9]*[.])[0]+'
+TEMP_REGEX = r'[+-]?([0-9]*[.])[0-9]+'
 
 # use 4 space delimiter
+
 sep = '\t'
 
 in_files = snakemake.input
-out_file = str(snakemake.output)
+out_file = snakemake.output
 
-with open(out_file, 'a') as f:
-    for in_file in in_files: 
-        print(in_file)
-        # TODO maybe avoid hardcoding the column number for elegance
-        mag_data = np.genfromtxt(in_file, skip_header=1, usecols=7) # m_z
+with open(str(out_file), 'a') as f:
+    for in_file in in_files:
+        # Skip the first 200 steps as equilibration. TODO: Use pymbar.
+        mag_data = np.genfromtxt(in_file, skip_header=201, usecols=7)
         mag = np.mean(mag_data)
 
         m = re.search(TEMP_REGEX, in_file)
         temp = float(in_file[m.start():m.end()])
-        print(str(temp))
 
         w = str(temp) + sep + str(round(mag, 3)) + "\n"
         f.write(w)
